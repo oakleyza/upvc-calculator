d8006580912c918895113bd6c32280666b48bdfa
Oakley <oakleyza@gmail.com>
2026-08-14 14:20:25 +0700

    feat: เปลี่ยน font เป็น Noto Sans Thai

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Noto Sans Thai"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
